7d063d9de2a315eb7cbc2056f1836343e9cc4921
Cosmin <cosmin.ciocan@savethechildren.org>
2021-10-25 20:45:33 +0300

    Lab 2

---
+++ src/Lab2.java
@@ -0,0 +1,23 @@
+public class Lab2 {
+    public static int x = 3;
+    public static int z = 3;
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        Print print = new Print();
+
+        double result = calc.sum(x, z);
+        System.out.println(result);
+
+        double substractResult = calc.substract(x, z);
+        System.out.println(substractResult);
+
+        double multiplyResult = calc.multiply(x, z);
+        System.out.println(substractResult);
+
+        print.printStars();
+
+    }
+
+
+}
