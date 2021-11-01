5a36bb8e8411af6d1eb0c17770ce3c6af487e8de
Cosmin <cosmin.ciocan@savethechildren.org>
2021-11-01 18:52:59 +0200

    Lab 2

--- src/Calculator.java
+++ src/Calculator.java
@@ -14,4 +14,20 @@ public class Calculator {
         int result = first * second;
         return result;
     }
+
+//    9.Scrieti o metoda java, care primeste distanta
+//    (in metrii) si timpul (ca si 3 numere: ore, minute, secunde),
+//    si afiseaza viteza, in metrii pe secunda, kilometrii pe ora si mile pe ora.
+//    (Indiciu: 1 mila = 1609 metrii)
+
+    public void printSpeedInMetric(float meters, int hours, int minutes, int seconds) {
+        // V = D/T
+        int timeInSeconds = (hours * 3600) + (minutes * 60) + seconds;
+        float speedMeters = meters / timeInSeconds;
+        System.out.println("The speed in m/s: "+speedMeters);
+        double kmPerHour = speedMeters * 3.6;
+        System.out.println("The speed in km/h: " + kmPerHour );
+        double milesPerHour = kmPerHour / 1.609;
+        System.out.println("The speed in m/h: " + milesPerHour);
+    }
 }
--- src/Lab2.java
+++ src/Lab2.java
@@ -4,18 +4,19 @@ public class Lab2 {
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        Print print = new Print();
-
-        double result = calc.sum(x, z);
-        System.out.println(result);
-
-        double substractResult = calc.substract(x, z);
-        System.out.println(substractResult);
-
-        double multiplyResult = calc.multiply(x, z);
-        System.out.println(multiplyResult);
-
-        print.printStars();
+//        Print print = new Print();
+//
+//        double result = calc.sum(x, z);
+//        System.out.println(result);
+//
+//        double substractResult = calc.substract(x, z);
+//        System.out.println(substractResult);
+//
+//        double multiplyResult = calc.multiply(x, z);
+//        System.out.println(multiplyResult);
+//
+//        print.printStars();
+        calc.printSpeedInMetric(10000,1,10,5);
 
     }
 
