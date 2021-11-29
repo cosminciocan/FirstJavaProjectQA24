f546e0746f4ae672a811a44c3afb9be9cd43d1fd
Cosmin <cosmin.ciocan@savethechildren.org>
2021-11-29 19:55:49 +0200

    Lab 7

---
+++ src/Lab7.java
@@ -0,0 +1,20 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Lab7 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ReadFromKeyboard read = new ReadFromKeyboard();
+        int x = read.getInt();
+        System.out.println("numarul este: " +x);
+
+
+        float a = scan.nextFloat();
+        String z = scan.next();
+        System.out.println("String citit este: " + z);
+        System.out.println("Float-ul citit este: " + a);
+
+
+    }
+}
---
+++ src/ReadFromKeyboard.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ReadFromKeyboard {
+
+
+    public int getInt() {
+        int count = 0;
+        do {
+            Scanner scan = new Scanner(System.in);
+            try {
+                System.out.print("Introdu un numar: ");
+                return scan.nextInt();
+            } catch (InputMismatchException error) {
+                System.out.println("Incorrect value!");
+                count++;
+            }
+            if (count == 5){
+               sleep(5);
+                count = 0;
+            }
+        } while (true);
+    }
+
+    private void sleep(int seconds){
+        System.out.print("Sleeping for: " );
+        do {
+            System.out.println(seconds);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            seconds --;
+        }while (seconds > 0);
+
+    }
+}
